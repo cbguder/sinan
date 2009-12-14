d099625868065b2fcbdb26cf544aaf61ac4b02f8
Can Berk Güder <cbguder@gmail.com>
2009-12-14 23:11:51 +0200

    Make all fields non-nullable, renew seeds.

--- db/migrate/20091022160115_create_cities.rb
+++ db/migrate/20091022160115_create_cities.rb
@@ -1,9 +1,9 @@
 class CreateCities < ActiveRecord::Migration
   def self.up
     create_table :cities do |t|
-      t.string :name
-      t.float :latitude
-      t.float :longitude
+      t.string :name, :null => false
+      t.float :latitude, :null => false
+      t.float :longitude, :null => false
     end
   end
 
--- db/migrate/20091022160133_create_buildings.rb
+++ db/migrate/20091022160133_create_buildings.rb
@@ -1,14 +1,14 @@
 class CreateBuildings < ActiveRecord::Migration
   def self.up
     create_table :buildings do |t|
-      t.string :name
-      t.integer :year
-      t.integer :city_id
-      t.text :address
-      t.text :directions
-      t.text :description
-      t.float :latitude
-      t.float :longitude
+      t.string :name, :null => false
+      t.integer :year, :null => false
+      t.integer :city_id, :null => false
+      t.text :address, :null => false
+      t.text :directions, :null => false
+      t.text :description, :null => false
+      t.float :latitude, :null => false
+      t.float :longitude, :null => false
 
       t.timestamps
     end
