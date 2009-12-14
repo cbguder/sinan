dcc8c62c7095ad46c2f35124ad899338bff72c66
Can Berk Güder <cbguder@gmail.com>
2009-12-14 23:13:13 +0200

    Rest of the skin, localization.

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,73 @@
+module AdminHelper
+  COLLECTION_ACTIONS = [ 'index', 'new' ]
+  MEMBER_ACTIONS     = [ 'show', 'edit', 'destroy' ]
+
+  T = {
+    'index'              => 'Listele',
+    'new'                => 'Yeni',
+    'create'             => 'Yeni',
+    'show'               => 'Görüntüle',
+    'edit'               => 'Düzenle',
+    'update'             => 'Güncelle',
+    'destroy'            => 'Sil',
+    'buildings'          => 'Eserler'
+  }
+
+  def t(str)
+    T[str] || str || ''
+  end
+
+  def controllers_menu(menu)
+    r = []
+    menu.each { |k|
+      link = case k
+        when controller.controller_name: link_to(t(k), { :controller => k }, { :class => 'current' })
+        else                             link_to(t(k), :controller => k)
+      end
+      r << content_tag(:li, link)
+    }
+    r.join('')
+  end
+
+  def controllers_select(menu)
+    r = []
+    menu.each { |k|
+        r << options_for_select(t(k) => k)
+    }
+    r.join('')
+  end
+
+  def actions_menu()
+    actions = COLLECTION_ACTIONS
+
+    if MEMBER_ACTIONS.include?(controller.action_name)
+      actions += MEMBER_ACTIONS
+    end
+
+    r = []
+    actions.each { |k|
+      link = case k
+        when 'destroy'             : link_to(t(k), { :action => k }, :confirm => 'Emin misiniz?', :method => :delete)
+        when controller.action_name: link_to(t(k), { :action => k }, { :class => 'current' })
+        else                         link_to(t(k), :action => k)
+      end
+      r << content_tag(:li, link)
+    }
+
+    r.join('')
+  end
+
+  def member_actions(object, methods)
+    links = []
+    controller = eval("#{object.class.to_s.pluralize}Controller.new")
+
+    for method in methods
+      if method == :destroy
+        links << link_to(t(method.to_s), object, :confirm => 'Emin misiniz?', :method => :delete)
+      else
+        links << link_to(t(method.to_s), polymorphic_path(object, :action => method))
+      end
+    end
+    links.join(' | ')
+  end
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function init() {
+	handleMessages();
+};
+
+function handleMessages() {
+	fades = $$(".fade");
+	fades.each(function(fade) {
+		if(fade.hasClassName('notice')) {
+			new Effect.Highlight(fade, { startcolor: '#99ff99', endcolor: '#e0ffe0', restorecolor: '#e0ffe0' });
+		} else if(fade.hasClassName('warning')) {
+			new Effect.Highlight(fade, { startcolor: '#ffff99', endcolor: '#ffffe0', restorecolor: '#ffffe0' });
+		} else if(fade.hasClassName('error')) {
+			new Effect.Highlight(fade, { startcolor: '#ff9999', endcolor: '#ffe0e0', restorecolor: '#ffe0e0' });
+		}
+	});
+	setTimeout("fades.invoke('fade')", 3000);
+}
